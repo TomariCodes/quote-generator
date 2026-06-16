814045a30fdc16a3c8a01cb393fe79fd2bace391
Tomari <tomari7878@gmail.com>
2026-06-16 13:49:21 -0500

    improved UX

--- src/main.js
+++ src/main.js
@@ -7,15 +7,13 @@ function generateQuote(event) {
   let prompt = `Generate a short quote about ${inputElement.value}`;
   let context =
     "You are a wise and insightful quote generator. Your task is to create a short and impactful quote based on the given keyword. If the same keyword is given then provide a different perspective or insight. The quote should be concise, thought-provoking, and relevant to the keyword provided. Use your creativity and wisdom to find a short quote that resonates with the reader and captures the essence of the keyword. Make sure to always credit the author/speaker.";
-  console.log("Generating quote...");
-  console.log(prompt);
-  console.log(context);
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<span class="blink">⌛Generating quote about ${inputElement.value}...</span>`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayQuote);
 }
 
 function displayQuote(response) {
-  console.log("Quote Generated");
   new Typewriter(quoteElement, {
     strings: response.data.answer,
     autoStart: true,
