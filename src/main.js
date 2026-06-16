5ce2f127443de3c4e9cbfda9513d29382cb709be
Tomari <tomari7878@gmail.com>
2026-06-16 12:26:08 -0500

    Added axios and intergrated api

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
-let inputElement = document.getElementById('keyword-input')
+let inputElement = document.getElementById("keyword-input");
 let quoteFormElement = document.getElementById("quote-generator-form");
 let quoteElement = document.getElementById(`quote-container`);
 function generateQuote(event) {
   event.preventDefault();
-  alert("Generating quote...");
+  let apiKey = "d723abdbd1batbf0d4fo3fa95586fbba";
+  let prompt = `Generate a short quote about ${inputElement.value}`;
+  let context =
+    "You are a wise and insightful quote generator. Your task is to create a short and impactful quote based on the given keyword. If the same keyword is given then provide a different perspective or insight. The quote should be concise, thought-provoking, and relevant to the keyword provided. Use your creativity and wisdom to find a short quote that resonates with the reader and captures the essence of the keyword. Make sure to always credit the author/speaker.";
+  console.log("Generating quote...");
+  console.log(prompt);
+  console.log(context);
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayQuote);
+}
+
+function displayQuote(response) {
+  console.log("Quote Generated");
   new Typewriter(quoteElement, {
-    strings: [`The only way to do great work is to love what you do. - Steve Jobs`, `Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill`, `Believe you can and you're halfway there. - Theodore Roosevelt`],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
